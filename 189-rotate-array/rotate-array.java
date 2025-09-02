f193d9d805ac388e2d019641c65630d5f1950884
Surajit Rana <surajit1602@gmail.com>
2025-09-02 11:49:00 +0530

    Time: 2 ms (25.10%) | Memory: 57.2 MB (86.47%) - LeetSync

---
+++ 189-rotate-array/rotate-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * Approach : Using Two Pointers Approach
+     *
+     * TC: O(3 x (N / 2)) ~ O(N)
+     * SC: O(1)
+     */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        if (k == 0) {
+            return;
+        }
+        reverse(nums, 0, n - 1); // TC: O(N / 2)
+        reverse(nums, 0, k - 1); // TC: O(N / 2)
+        reverse(nums, k, n - 1); // TC: O(N / 2)
+    }
+
+    /**
+     * Using Two Pointers Approach
+     *
+     * TC: O(N / 2)
+     * SC: O(1)
+     */
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[end];
+            nums[end] = nums[start];
+            nums[start] = temp;
+            start++;
+            end--;
+        }
+    }
+}
