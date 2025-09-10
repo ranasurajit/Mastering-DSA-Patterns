1885e61530681a75ec642b92849aa5f88d8e57f3
Surajit Rana <surajit1602@gmail.com>
2025-09-10 08:26:37 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (46.99%) - LeetSync

--- P02_Binary_Search/1D_Arrays/792-binary-search/binary-search.java
+++ P02_Binary_Search/1D_Arrays/792-binary-search/binary-search.java
@@ -1,11 +1,53 @@
 class Solution {
     /**
-     * Approach I : Using Iterative Binary Search
+     * Approach II : Using Recursive Binary Search (Divide and Conquer) Approach
+     *
+     * TC: O(log(N))
+     * SC: O(log(N))
+     */
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        return searchRecursion(nums, target, 0, n - 1); // TC: O(log(N)), SC: O(log(N))
+    }
+
+    /**
+     * Using Recursive Binary Search (Divide and Conquer) Approach
+     *
+     * TC: O(log(N))
+     * SC: O(log(N)) - recursion stack
+     */
+    private int searchRecursion(int[] nums, int target, int low, int high) {
+        // Base Case
+        if (low > high) {
+            return -1;
+        }
+        if (low == high) {
+            if (nums[low] == target) {
+                return low;
+            } else {
+                return -1;
+            }
+        }
+        // Recursion Calls
+        int mid = low + (high - low) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            // search in it 2nd half
+            return searchRecursion(nums, target, mid + 1, high);
+        } else {
+            // search in it 1st half
+            return searchRecursion(nums, target, low, mid - 1);
+        }
+    }
+
+    /**
+     * Approach I : Using Iterative Binary Search Approach
      *
      * TC: O(log(N))
      * SC: O(1)
      */
-    public int search(int[] nums, int target) {
+    public int searchIterative(int[] nums, int target) {
         int n = nums.length;
         int low = 0;
         int high = n - 1;
